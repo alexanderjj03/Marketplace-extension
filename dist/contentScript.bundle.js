53f297a21bf96b5d3861278ff1a99b0a575eecc3
xanderj <xanderj@student.ubc.ca>
2025-08-27 14:03:59 -0700

    Scrape individual listing is done, analyzing these results is next

--- dist/contentScript.bundle.js
+++ dist/contentScript.bundle.js
@@ -162,9 +162,9 @@
     const attributeElems = inline.children[1].children[0].children[1].children[0].children[1 - pageConfig].children[0];
     // No identifiers beyond changing class names to go off of.
     // This will do for now but we will need a more stable solution.
-    console.log(attributeElems.children.length);
 
-    getAttributes(attributeElems.children); // attributes should be a dictionary.
+    let attributes = getAttributes(attributeElems.children); // attributes should be a dictionary.
+    console.log(attributes);
     conclusion = analyzeAttrs();
 
     return conclusion;
@@ -192,7 +192,20 @@
     const postedDate = targetNode.querySelector('[dir="auto"]').children[0];
     attrs["date"] = postedDate.querySelector('abbr').getAttribute('aria-label');
 
-    // more stuff: description, user join year, condition (if present) user marketplace rating (if any)
+    attrs["description"] = extractText(elems[0].children[4].children[0].children[1].children[1]);
+    attrs["condition"] = "N/A";
+
+    let conditionNode = elems[0].children[4].children[0].children[1].children[0];
+    conditionNode.childNodes.forEach((row) => {
+      let labels = row.querySelectorAll('[dir="auto"]');
+      if (labels[0].textContent.toLowerCase() === "condition") {
+        attrs["condition"] = labels[1].textContent.toLowerCase();
+      }
+    });
+
+    let userInfo = extractUserInfo(elems[1]);
+    attrs["user join year"] = userInfo[0];
+    attrs["user rating"] = userInfo[1];
 
     return attrs
   }
@@ -202,19 +215,58 @@
     const postedDate = targetNode.querySelector('[dir="auto"]').children[0];
     attrs["date"] = postedDate.querySelector('abbr').getAttribute('aria-label');
 
-    // more stuff: description, user join year, condition (if present) user marketplace rating (if any)
+    attrs["description"] = extractText(elems[5].children[1].children[0].children[0]);
+    // Mutation observer needed
+
+    let driven = extractText(elems[4].children[1]);
+    attrs["distance driven"] = driven.toLowerCase().split("driven ")[1];
+
+    let userInfo = extractUserInfo(elems[6]);
+    attrs["user join year"] = userInfo[0];
+    attrs["user rating"] = userInfo[1];
 
     return attrs
   }
 
   function getProperty(attrs, elems) {
-    let targetNode = elems[0].children[2];
-    let type = targetNode.querySelector('[dir="auto"]').textContent;
+    let type = extractText(elems[0].children[2]);
     attrs["type"] = ((type.toLowerCase() === "home sales") ? "property sale" : "property rental");
+    let listedText = extractText(elems[1].children[0].children[1]);
+    attrs["date"] = (listedText.toLowerCase().split(" ago")[0]).split("listed ")[1];
 
-    // more stuff: date, description, user join year, condition (if present) user marketplace rating (if any)
+    attrs["description"] = extractText(elems[7].children[1].children[0].children[0]);
+    // Mutation observer needed
+
+    let userInfo = extractUserInfo(elems[13]);
+    attrs["user join year"] = userInfo[0];
+    attrs["user rating"] = userInfo[1];
 
     return attrs
+  }
+
+  // starts just above aria-hidden = false
+  // PROBLEM: Only the shortened form of the description is extracted (the user has to press the "see more" button).
+  // Not too important of an issue for now.
+  function extractText(elem) {
+    // Implement a mutation observer for the text content. NOT NEEDED for general listings (only vehicle/property)
+    const target = elem.querySelector('[dir="auto"]');
+    return target.textContent;
+  }
+
+  function extractUserInfo(elem) {
+    let ret = ["", "no"];
+    let list = elem.querySelector('[role="list"]').childNodes;
+
+    for (let i = 1; i < list.length; i++) {
+      let text = extractText(list[i]).toLowerCase();
+      if (text.includes("highly rated ")) {
+        ret[1] = "yes";
+      } else if (text.includes("joined facebook ")) {
+        ret[0] = parseInt(text.split("in ")[1]);
+      }
+    }
+
+    return ret // user join year, user marketplace rating (yes/no = "highly rated"/not highly rated)
   }
 
   function analyzeAttrs(attributes) {
